4c2bff21e1571b9049cf6771e4bb4c26f4d77b8b
Vinicius <viniciustfaraujo@gmail.com>
2022-11-25 22:46:20 -0300

    add pandas loc method example and plotly pie chart example

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 header = st.container()
 dataset = st.container()
@@ -21,3 +22,18 @@ with dataset:
     gender = pd.DataFrame(dropout_data['Gender'].value_counts())
     st.bar_chart(gender)
     # O gráfico desse bloquinho acima de 3 linhas ficou estranho com o gráfico do streamlit. Ver como fazer no ploty
+    
+    st.title("Testando plotly e manipulação de valores de colunas no pandas!")
+    st.subheader("Aqui trocamos valores '1' e '0' da coluna 'Gender' por valores 'Male' e 'Female' usando o método pandas.DataFrame.loc")
+
+    dropout_data.loc[dropout_data['Gender'] == 1, "Gender"] = 'Male'
+    dropout_data.loc[dropout_data['Gender'] == 0, "Gender"] = 'Female'
+
+    st.write(dropout_data.head())
+    
+    df = dropout_data.groupby(['Gender'])['Gender'].count().reset_index(name='count')
+    st.title('Gender of students')
+    st.subheader('Gráfico feito com plotly')
+    fig = px.pie(df, values='count', names='Gender')
+
+    st.write(fig)
