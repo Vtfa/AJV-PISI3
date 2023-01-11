b710759132921aeb53aa4e2e6ddc2a802f9f550d
Vinicius <viniciustfaraujo@gmail.com>
2023-01-11 20:23:51 -0300

    add necessary imports for page2 and a graph example

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
 
 header = st.container()
 dataset = st.container()
@@ -9,3 +15,7 @@ with header:
 with dataset:
     dropout_data = st.session_state['dropout_data_state']
     st.write(dropout_data)
+
+
+    bar_age = pd.DataFrame(dropout_data['Age at enrollment'].value_counts())
+    st.bar_chart(bar_age)
