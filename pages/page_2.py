c08c9dc3cc5afd5450e959a8dce06861908dd354
Vinicius <viniciustfaraujo@gmail.com>
2023-02-12 22:03:00 -0300

    add sample box graph, and funnel graph of total students and target

--- pages/page_2.py
+++ pages/page_2.py
@@ -19,3 +19,33 @@ with dataset:
 
     bar_age = pd.DataFrame(dropout_data['Age at enrollment'].value_counts())
     st.bar_chart(bar_age)
+
+    #box graph
+    box_sample = dropout_data.sample(n=50, random_state=66)
+    box_age = px.box(box_sample, y='Age at enrollment', points='all')
+    st.write(box_age)
+
+    #auxiliar df with students and target totals
+    status_totals = dropout_data.groupby('Target').size()
+    total_students = len(dropout_data)
+
+    result_df = pd.DataFrame({'total_students': [total_students]})
+    result_df = pd.concat([result_df, status_totals.to_frame().T], axis=1)
+
+    st.write(result_df)
+
+    # Create the Funnel Graph
+    funnel_total = go.Figure(go.Funnel(
+        y=["Total Students", "Graduate", "Dropout", "Enrolled"],
+        x=[total_students, status_totals["Graduate"], status_totals["Dropout"], status_totals["Enrolled"]],
+        textinfo="value+percent total",
+        marker={"color": ["#FFA07A", "#87CEEB", "#98FB98", "#DDA0DD"]}
+    ))
+
+    # Add title and labels
+    funnel_total.update_layout(
+        title="Student Status Funnel Graph",
+        yaxis_title="Student Status"
+    )
+
+    st.write(funnel_total)
