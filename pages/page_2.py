8f5483a0c23b36f2802c4cc24e2e1158dc78bf11
Vinicius <viniciustfaraujo@gmail.com>
2023-02-13 22:25:11 -0300

    add funnel graph for female and male students

--- pages/page_2.py
+++ pages/page_2.py
@@ -29,23 +29,58 @@ with dataset:
     status_totals = dropout_data.groupby('Target').size()
     total_students = len(dropout_data)
 
+    female_data = dropout_data[dropout_data['Gender'] == 'Female']
+    female_target_totals = female_data.groupby('Target').size()
+    female_students_total = len(female_data)
+
+    male_data = dropout_data[dropout_data['Gender'] == 'Male']
+    male_target_totals = male_data.groupby('Target').size()
+    male_students_total = len(male_data)
+
     result_df = pd.DataFrame({'total_students': [total_students]})
     result_df = pd.concat([result_df, status_totals.to_frame().T], axis=1)
 
     st.write(result_df)
 
-    # Creates Funnel Graph
     funnel_total = go.Figure(go.Funnel(
         y=["Total Students", "Graduate", "Dropout", "Enrolled"],
         x=[total_students, status_totals["Graduate"], status_totals["Dropout"], status_totals["Enrolled"]],
         textinfo="value+percent initial",
-        marker={"color": ["#FFA07A", "#87CEEB", "#98FB98", "#DDA0DD"]}
+        marker={"color": ["#FFA07A", "#98FB98", "#FF6961", "#87CEEB"]}
+        #marker={"color": ["#FFA07A", "#87CEEB", "#98FB98", "#DDA0DD"]}
     ))
 
-    # Funnel's title and labels
     funnel_total.update_layout(
         title="Student Status Funnel Graph",
         yaxis_title="Student Status"
     )
 
     st.write(funnel_total)
+
+    funnel_female = go.Figure(go.Funnel(
+        y=["Total Students", "Graduate", "Dropout", "Enrolled"],
+        x=[female_students_total, female_target_totals["Graduate"], female_target_totals["Dropout"], female_target_totals["Enrolled"]],
+        textinfo="value+percent initial",
+        marker={"color": ["#FFA07A", "#87CEEB", "#98FB98", "#DDA0DD"]}
+    ))
+
+    funnel_female.update_layout(
+        title="Student Status Funnel Graph",
+        yaxis_title="Student Status"
+    )
+
+    st.write(funnel_female)
+
+    funnel_male = go.Figure(go.Funnel(
+        y=["Total Students", "Dropout", "Graduate", "Enrolled"],
+        x=[male_students_total, male_target_totals["Dropout"], male_target_totals["Graduate"], male_target_totals["Enrolled"]],
+        textinfo="value+percent initial",
+        marker={"color": ["#FFA07A", "#87CEEB", "#98FB98", "#DDA0DD"]}
+    ))
+
+    funnel_male.update_layout(
+        title="Student Status Funnel Graph",
+        yaxis_title="Student Status"
+    )
+
+    st.write(funnel_male)
