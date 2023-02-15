d8d0d0732e0d15e56cc2da7eb2ab4149690d09d3
AF <aldemar.poli@gmail.com>
2023-02-15 12:00:44 -0300

    added demographic pyramid plot

--- plot_funcs.py
+++ plot_funcs.py
@@ -26,3 +26,61 @@ def gender_tree(course_data: pd.DataFrame):
     )
 
     return st.plotly_chart(gender_tree, use_container_width=True)
+
+
+def demographic_pyramid(data: pd.DataFrame):
+    dem_pyramid = go.Figure()
+    dem_pyramid.add_trace(
+        go.Bar(
+            x=data['Male'],
+            y=data.index,
+            orientation='h',
+            name='Male',
+            marker={
+                'color': 'rgba(0, 204, 150, 0.5)'
+            },
+            hoverinfo='x',
+        )
+    )
+
+    dem_pyramid.add_trace(
+        go.Bar(
+            x=data['Female'] * -1,
+            y=data.index,
+            text=data['Female'],
+            textfont_color='rgba(0, 0, 0, 0)',
+            orientation='h',
+            name='Female',
+            marker={
+                'color': 'rgba(99, 110, 250, 0.5)'
+            },
+            hoverinfo='text',
+        )
+    )
+
+    dem_pyramid.update_layout(
+        title="Students' Population Pyramid",
+        title_font_size=22,
+        font_size=16,
+        barmode = 'relative',
+        bargap=0.15,
+        xaxis={
+            'tickvals': [-1500, -1000, -500, 0, 500, 1000, 1500],
+        'ticktext':
+            ['1.500', '1.000', '500', '0', '500', '1.000', '1.500']
+        },
+        xaxis_title='# of students',
+        yaxis_title='Age range',
+        height=700,
+        margin_pad=5,
+        margin={'l': 90},
+        hovermode='y',
+        plot_bgcolor='rgba(0,0,0,0)',
+    )
+
+    dem_pyramid.update_yaxes(
+        gridcolor='rgba(0, 0, 0, 0.10)',
+        title_standoff=20,
+    )
+
+    st.plotly_chart(dem_pyramid, use_container_width=True)
