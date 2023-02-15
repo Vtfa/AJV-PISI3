ebd64aa71ac9828e6c590886a5f26c2d79f2c779
AF <aldemar.poli@gmail.com>
2023-02-15 19:42:45 -0300

    added config_page func

--- aux_funcs.py
+++ aux_funcs.py
@@ -7,3 +7,20 @@ def valid_session_data(vars: list[str], msg: str) -> bool:
             return False
 
     return True
+
+
+def config_page(title: str) -> None:
+    st.set_page_config(
+        page_title=title,
+        layout='wide',
+        page_icon=u"\U0001F393",
+        menu_items={
+            'About': '''
+            #### **Equipe**
+            Aldemar S R Filho\n
+            Arthur de Barros Botelho dos Santos\n
+            Douglas Rafael Miranda de Souza\n
+            João Vitor da Silva Pires\n
+            Vinicius Thalles Ferreira Araujo''',
+        }
+    )
