5e39e66393dabd5ab5626b1129040ff2eb4c68d7
João Vitor da Silva Pires <94564034+JVitorAccelerator@users.noreply.github.com>
2023-03-06 20:11:52 -0300

    Sidebar

--- plot_funcs.py
+++ plot_funcs.py
@@ -1,195 +1,205 @@
+import time
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-
-from plotly.subplots import make_subplots
 
 
-def gender_tree(data: pd.DataFrame) -> None:
-    gender_tree = px.treemap(
-        data,
-        title="Gender distribution by course",
-        path=["Course", "Gender"],
-        color_continuous_scale="RdBu",
-        color="count",
-        values="count",
-        height=1000,
+def config_page(title: str) -> None:
+    st.set_page_config(
+        page_title=title,
+        layout='wide',
+        page_icon=u"\U0001F393",
+        menu_items={
+            'About': '''
+            #### **Equipe**
+            Aldemar S R Filho\n
+            Arthur de Barros Botelho dos Santos\n
+            Douglas Rafael Miranda de Souza\n
+            João Vitor da Silva Pires\n
+            Vinicius Thalles Ferreira Araujo''',
+        }
     )
 
-    gender_tree.data[0].textinfo = "label+value+percent parent+percent entry+percent root"
 
-    gender_tree.update_layout(
-        title_font_size=26,
-        font_size=16,
+def page_style(menu_title: str = '#AJV'):
+    st.markdown(f"""
+        <style>
+        .justified-text {{
+            text-align: justify;
+        }}
+        .content-size {{
+            font-size:1.05em !important;
+        }}
+        .indent-text {{
+            text-indent: 40px;
+        }}
+        .italic {{
+            font-style: italic;
+        }}
+        .streamlit-expanderHeader {{
+            font-size: 1.05em;
+            font-weight: bold;
+            color: IndianRed;
+        }}
+        [data-testid="stSidebarNav"]::before {{
+            content: "{menu_title}";
+            color: indianred;
+            font-weight: bold;
+            margin-left: 20px;
+            font-size: 30px;
+            position: relative;
+            top: 100px;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
     )
 
-    st.plotly_chart(gender_tree, use_container_width=True)
+
+def valid_session_data(dataframes: list[str], message: str, sleep_time: float = 0.35) -> bool:
+    progress_bar = st.progress(0)
+    step = 1 / len(dataframes)
+
+    df_exists = {}
+    for i, df in enumerate(dataframes, start=1):
+        progress_bar.progress(step * i)
+        time.sleep(sleep_time)
+
+        if df not in st.session_state:
+            df_exists[df] = False
+            continue
+
+        df_exists[df] = True
+
+    time.sleep(sleep_time)
+    progress_bar.empty()
+
+    if not all(df_exists.values()):
+        st.markdown(message, unsafe_allow_html=True)
+        with st.expander('Errors', True):
+            for df, exists in df_exists.items():
+                if not exists:
+                    st.error(f'⚠️ Could not find _**{df}**_ data')
+
+        return False
+
+    return True
 
 
-def demographic_pyramid(data: pd.DataFrame):
-    dem_pyramid = go.Figure()
-    dem_pyramid.add_trace(
-        go.Bar(
-            x=data["Male"],
-            y=data.index,
-            orientation="h",
-            name="Male",
-            marker={"color": "rgba(0, 204, 150, 0.5)"},
-            hoverinfo="x",
+def sidebar_01():
+    data: pd.DataFrame = st.session_state['dropout_data'][[
+        'Marital status',
+        'Course',
+        'Gender',
+        'age_range',
+        'Escolaridade mae',
+        'Escolaridade pai',
+    ]]
+
+    options = {column: data[column].dropna().unique().tolist()
+            for column in data}
+
+    multiselect_fields = ['Gender', 'age_range']
+
+    for field, option in options.items():
+        if field not in multiselect_fields:
+            option.append('')
+            option.sort()
+
+    with st.sidebar:
+        st.header('Dataset table configuration')
+
+        with st.expander('Filtros', True):
+            with st.form("dataset_form"):
+                marital_status =  st.selectbox(
+                    'Estado Civil',
+                    options['Marital status'],
+                    help='Define the gender to be filtered at dataset',
+                )
+
+                course =  st.selectbox(
+                    'Curso',
+                    options['Course'],
+                    help='Define the course to be filtered at dataset',
+                )
+
+                gender =  st.multiselect(
+                    'Gender',
+                    options['Gender'],
+                    help='Define the gender to be filtered at dataset',
+                )
+
+                age_range =  st.multiselect(
+                    'Faixa etária',
+                    options['age_range'],
+                    help='Define the age range to be filtered at dataset',
+                )
+
+                escolaridade_mae =  st.selectbox(
+                    'Escolaridade da mãe',
+                    options['Escolaridade mae'],
+                    help='Define the mothers schooling to be filtered at dataset',
+                )
+
+                escolaridade_pai =  st.selectbox(
+                    'Escolaridade do pai',
+                    options['Escolaridade pai'],
+                    help='Define the fathers schooling to be filtered at dataset',
+                )
+
+                submitted = st.form_submit_button('Filtrar')
+
+                if submitted:
+                    st.session_state['marital_status'] = marital_status
+                    st.session_state['course'] = course
+                    st.session_state['gender'] = gender
+                    st.session_state['age_range'] = age_range
+                    st.session_state['escolaridade_mae'] = escolaridade_mae
+                    st.session_state['escolaridade_pai'] = escolaridade_pai
+
+        st.header('Plots configuration')
+        st.session_state['gender_select'] = st.selectbox(
+            'Gender',
+            ['Female', 'Male'],
+            help='Define the gender to be used at gender related plots',
         )
-    )
-
-    dem_pyramid.add_trace(
-        go.Bar(
-            x=data["Female"] * -1,
-            y=data.index,
-            text=data["Female"],
-            textfont_color="rgba(0, 0, 0, 0)",
-            orientation="h",
-            name="Female",
-            marker={"color": "rgba(99, 110, 250, 0.5)"},
-            hoverinfo="text",
-        )
-    )
-
-    dem_pyramid.update_layout(
-        title="Students' Population Pyramid",
-        title_font_size=22,
-        font_size=16,
-        barmode="relative",
-        bargap=0.15,
-        xaxis={
-            "tickvals": [-1500, -1000, -500, 0, 500, 1000, 1500],
-            "ticktext": ["1.500", "1.000", "500", "0", "500", "1.000", "1.500"],
-        },
-        xaxis_title="# of students",
-        yaxis_title="Age range",
-        height=700,
-        margin_pad=5,
-        margin={"l": 90},
-        hovermode="y",
-        plot_bgcolor="rgba(0,0,0,0)",
-    )
-
-    dem_pyramid.update_yaxes(
-        gridcolor="rgba(0, 0, 0, 0.10)",
-        title_standoff=20,
-    )
-
-    st.plotly_chart(dem_pyramid, use_container_width=True)
-
-
-def specific_gender_tree(data: pd.DataFrame, gender: str) -> None:
-    specific_gender_tree = px.treemap(
-        data.query(f'Gender == "{gender}"'),
-        title=f"Course and age distribution for {gender} students",
-        path=["Gender", "Course", "age_range"],
-        values="count",
-        height=1000,
-    )
-
-    specific_gender_tree.data[0].textinfo = "label+value+percent parent+percent entry+percent root"
-
-    specific_gender_tree.update_layout(
-        title_font_size=26,
-        font_size=16,
-    )
-
-    st.plotly_chart(specific_gender_tree, use_container_width=True)
-
-
-def gender_by_course(data: pd.DataFrame) -> None:
-    course_gender_age = go.Figure()
-    course_gender_age_index = (
-        data["Course"]
-        .str.replace("(", " ", regex=False)
-        .str.replace(" ", "<br>")
-        .str.replace(")", "", regex=False)
-        .unique()
-    )
-    course_gender_age_range = [0, 650]
-
-    course_gender_data = data.groupby(["Course", "Gender"])[["count"]].sum()
-    course_gender_data = course_gender_data.unstack().droplevel(0, 1)
-
-    course_gender_age.add_trace(
-        go.Bar(
-            name="Female",
-            x=course_gender_age_index,
-            y=course_gender_data["Female"],
-            orientation="v",
-            marker={"color": "rgba(99, 110, 250, 0.5)"},
-        ),
-    )
-
-    course_gender_age.add_trace(
-        go.Bar(
-            name="Male",
-            x=course_gender_age_index,
-            y=course_gender_data["Male"],
-            orientation="v",
-            marker={"color": "rgba(0, 204, 150, 0.5)"},
-        ),
-    )
-
-    course_gender_age.update_xaxes(
-        title_text="Course",
-        title_standoff=35,
-        tickangle=-90,
-    )
-
-    course_gender_age.update_yaxes(
-        title_text="Students",
-        title_standoff=25,
-    )
-
-    course_gender_age.update_layout(
-        height=900,
-        font_size=14,
-        title="Gender distribution by course",
-        barmode="stack",
-        hovermode="x unified",
-        margin={"b": 190},
-        margin_pad=10,
-    )
-
-    st.plotly_chart(course_gender_age, use_container_width=True)
-
-
-def dropout_by_gender(df: pd.DataFrame) -> None:
-    debt_gender_tree = px.treemap(
-                df,
-                title='Target distribution by gender',
-                path=['Gender', 'Target', 'debt'],
-                values='count',
-                height=1000,
-        )
-
-    debt_gender_tree.data[0].textinfo = 'label+value+percent parent+percent entry+percent root'
-
-    debt_gender_tree.update_layout(
-        title_font_size=26,
-        font_size=16,
-    )
-
-    st.plotly_chart(debt_gender_tree, use_container_width=True)
-
-
-def dropout_by_age_debt(df: pd.DataFrame) -> None:
-    debt_tree = px.treemap(
-                df,
-                title='Target distribution by age and debt',
-                path=['age_range', 'Target', 'debt'],
-                values='count',
-                height=1000,
+        st.session_state['age_interval'] = st.number_input(
+            'Age range interval',
+            step=1,
+            help='Define the interval (in years) to be used at demographic plots',
         )
 
-    debt_tree.data[0].textinfo = 'label+value+percent parent+percent entry+percent root'
 
-    debt_tree.update_layout(
-        title_font_size=26,
-        font_size=16,
-    )
 
-    st.plotly_chart(debt_tree, use_container_width=True)
+def sidebar_page3():
+    data: pd.DataFrame = st.session_state['dropout_data'][[
+        'Marital status',
+        'Course',
+        'Gender',
+        'age_range',
+        'Escolaridade mae',
+        'Escolaridade pai',
+    ]]
+
+    options = {column: data[column].dropna().unique().tolist()
+            for column in data}
+
+    multiselect_fields = ['Gender', 'age_range']
+
+    for field, option in options.items():
+        if field not in multiselect_fields:
+            option.append('')
+            option.sort()
+
+    with st.sidebar:
+
+        st.header('Plots configuration')
+        st.session_state['gender_select'] = st.selectbox(
+            'Gender',
+            ['Female', 'Male'],
+            help='Define the gender to be used at gender related plots',
+        )
+        st.session_state['age_interval'] = st.number_input(
+            'Age range interval',
+            step=1,
+            help='Define the interval (in years) to be used at demographic plots',
+        )
