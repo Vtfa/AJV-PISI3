3c9e5f92370b55acf4082267a3d992db55fdd85e
João Vitor da Silva Pires <94564034+JVitorAccelerator@users.noreply.github.com>
2023-03-07 14:42:08 -0300

    New plots old plots functions

--- plot_funcs.py
+++ plot_funcs.py
@@ -213,3 +213,46 @@ def dropout_histogram():
     st.subheader("Histograma de evasão por curso")   
     histograma_drop= px.histogram(Dropout, x="Course", color="Target",barnorm = "percent",text_auto= True, color_discrete_sequence=["mediumvioletred", "lightblue", "pink"],).update_layout(title={"text": "Percent :Course - Target","x": 0.5},yaxis_title="Percent").update_xaxes(categoryorder='total descending')
     st.write(histograma_drop)
+
+
+def grade_semesters():
+    Dropout = pd.read_csv('data/dropout.csv')
+    st.subheader("Distribuição de notas por curso (Double-click para isolar") 
+    box_1stSemester= px.box (Dropout_data.sort_values(by='Course'),  x="Course" , y="Curricular units 1st sem (grade)", color= "Course")
+    box_2ndSemester= px.box (Dropout_data.sort_values(by='Course'),  x="Course" , y="Curricular units 2nd sem (grade)", color= "Course")
+    st.subheader("Primeiro semestre")
+    st.write(box_1stSemester)
+    st.subheader("Segundo semestre")
+    st.write(box_2ndSemester)
+
+    st.subheader("Primeiro Semestre")
+    bar_1stSemester= px.bar(Dropout_data.sort_values(by='Course'), x="Course", y="Curricular units 1st sem (grade)")
+    st.write(bar_1stSemester)
+    st.subheader("Segundo Semestre")
+    bar_2ndSemester= px.bar(Dropout_data.sort_values(by='Course'), x="Course", y="Curricular units 2nd sem (grade)")
+    st.write(bar_2ndSemester)
+
+    scatter= px.scatter(Dropout_data, x= "Course", y="Curricular units 1st sem (grade)" , color= "Target" )
+    st.write(scatter) 
+    scatter= px.scatter(Dropout_data, x= "Course", y="Curricular units 2nd sem (grade)" , color= "Target" )
+    st.write(scatter) 
+
+
+def gender_course():
+    Dropout = pd.read_csv('data/dropout.csv')
+    Gender_Map= {0:'Female', 1: 'Male'}
+    Dropout_data['Gender'] = Dropout_data['Gender'].map(Gender_Map)
+    st.subheader("Porcentagem de estado de estudante por gênero")   
+    Gender_PercentBar= px.histogram( Dropout_data.sort_values(by='Gender'), x="Gender", color="Target",barnorm = "percent",text_auto= True, color_discrete_sequence=["mediumvioletred", "lightblue", "pink"],).update_layout(title={"text": "Percent :Course - Gender","x": 0.5},yaxis_title="Percent").update_xaxes(categoryorder='total descending')
+    st.write(Gender_PercentBar)
+
+    Gender_Bar = px.bar(Dropout_data.sort_values(by='Gender'), x="Course", color= "Gender",barmode= "group" ,text_auto= True, color_discrete_sequence=["pink", "lightblue"],)
+    st.subheader("Distribuição de gênero de studantes por curso")     
+    Gender_Bar.update_layout(title= "Numéro de estudantes por curso", xaxis_title="Cursos", yaxis_title="Número de estudantes")  
+    st.write(Gender_Bar)
+        
+
+    Financial_Status= px.histogram(Dropout_data.sort_values(by='Course'), x= "Course", y= "Renda total", color="Course", barnorm= "percent")
+    st.subheader("Índice de formação por renda total")
+    Financial_Status.update_layout(xaxis_title="Cursos", yaxis_title= "Renda total")
+    st.write(Financial_Status)
