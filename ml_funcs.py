7e08d4a74d86bbda5168d7681e7908173b3c9a2e
AF <aldemar.poli@gmail.com>
2023-03-24 19:24:00 -0300

    fix

--- ml_funcs.py
+++ ml_funcs.py
@@ -1,19 +1,18 @@
 import logging
 import os
 import pickle
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.svm import SVC
 
 
-def is_model_trained(path: str) -> bool:
-    return os.path.isfile(path)
+def train_model(model_instance,
+                x_train,
+                y_train,
+                preffix: str,
+                dir='./models'):
 
-
-def train_model(model_instance, x_train, y_train, preffix: str, dir='./models'):
     model_name = f'{preffix}_model'
     model_path = os.path.join(dir, model_name)
 
-    if is_model_trained(model_path):
+    if os.path.isfile(model_path):
         with open(model_path, 'rb') as file:
             model = pickle.load(file)
 
